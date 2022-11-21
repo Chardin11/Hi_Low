aca2f6f8a84b5a2f208ea7c09deb4b837aca87bb
Connor Hardin <Connorhardin@gmail.com>
2022-11-21 11:06:12 -0600

    fixed small issues

--- controller.java
+++ controller.java
@@ -22,7 +22,7 @@ public class controller{
         String[] playerNames = {"Player1", "Player2", "Player3", "Player4", "Player5", "Player6", "Player7", "Player8", "Player9", "Player10", "Player11", "Player12", "Player13", "Player14", "Player15"};
         Scanner nameinput = new Scanner(System.in);
         for (int i = 0; i < playerCount; i++) {
-            System.out.println("Enter in the player names:");
+            System.out.println("Enter in the player name:");
             String playerIds = nameinput.nextLine();
             playerNames[i] = playerIds;
         }
@@ -56,12 +56,12 @@ public class controller{
                 playerBets(currencyAmount, playerBets, true);
                 
                 currencyAmount = playerBets(currencyAmount, playerBets, dAnswer);
-                System.out.println(playerNames[i] + " your new betting amount: " + currencyAmount);
-                
-                Rinput.close();
-                Sinput.close();
-                bInput.close();
-                currencyInout.close();        
+                System.out.println("\n" + playerNames[i] + " your new betting amount: " + currencyAmount);
+                System.out.println(" ");
+                // Rinput.close();
+                // Sinput.close();
+                // bInput.close();
+                // currencyInout.close();        
 
         }
         input.close();
